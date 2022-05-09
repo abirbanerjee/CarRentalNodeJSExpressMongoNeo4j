2ba046ebaee46d8d66720815057599e1bf998463
abirbanerjee <abir.banerjee@gmail.com>
2022-05-09 11:57:52 +0200

    Bug fixes

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ app.use(express.static('web'));
 
 const neo4j = require('neo4j-driver');
 const uri = 'bolt://localhost:7687';
-const user='neo4j';
+const user = 'neo4j';
 const password = 'admin';
 const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
 
 let resultJson;
 
 // session.run('MATCH (P) RETURN(P)').then(result=>resultJson=result).then(driver.close());
-app.post('/nodes',async (req,res)=>{
-    
+app.post('/nodes', async (req, res) => {
+
     const lat = (req.body.lat).toFixed(3);
     const lon = (req.body.lon).toFixed(3);
-    console.log(lat,lon);
+    console.log(lat, lon);
     const session = driver.session();
     const ses = await session.run(`MATCH (p:Car)
     WITH point({longitude:toFloat(p.lon), latitude:toFloat(p.lat)}) as p1, point({longitude:toFloat(${lon}),latitude:toFloat(${lat})}) as p2,p
@@ -29,17 +29,17 @@ app.post('/nodes',async (req,res)=>{
     await session.close();
 })
 
-app.post('/cardetails',async (req,res)=>{
+app.post('/cardetails', async (req, res) => {
     const car_id = req.body.id;
     const filterFrom = req.body.filterFrom;
     const filterTo = req.body.filterTo;
     const carDetails = await MongoFunctions.avlbl(car_id, filterFrom, filterTo);
-    let data ={};
+    let data = {};
     data.carDetails = carDetails;
     res.send(data);
 })
 
-app.post('/fetchuser', async(req,res)=>{
+app.post('/fetchuser', async (req, res) => {
     const phone = req.body.userPhone;
     const userDetails = await MongoFunctions.srchCust(phone);
     let data = {};
@@ -47,7 +47,7 @@ app.post('/fetchuser', async(req,res)=>{
     res.send(data);
 })
 
-app.post('/newuser', async(req,res)=>{
+app.post('/newuser', async (req, res) => {
     const fname = req.body.fname;
     const lname = req.body.lname;
     const phone = req.body.phone;
@@ -56,70 +56,70 @@ app.post('/newuser', async(req,res)=>{
     res.send(status);
 })
 
-app.post('/newbooking', async(req,res)=>{
+app.post('/newbooking', async (req, res) => {
     const phone = req.body.userPhone;
     const car_id = req.body.carID;
     const from = req.body.from;
     const to = req.body.to;
     const pickupCode = req.body.carPickupCode;
-    const status = await MongoFunctions.newBooking(car_id,phone,from,to, pickupCode);
+    const status = await MongoFunctions.newBooking(car_id, phone, from, to, pickupCode);
     res.send(status);
 })
 
-app.post('/custbookings', async(req,res)=>{
+app.post('/custbookings', async (req, res) => {
     const phone = req.body.userPhone;
-    let resBooking =[];
+    let resBooking = [];
     let bookings = await MongoFunctions.srchBooking(phone);
-    for(let i=0;i<bookings.length;i++){
-        const stamp =(bookings[i]._id).getTimestamp();
+    for (let i = 0; i < bookings.length; i++) {
+        const stamp = (bookings[i]._id).getTimestamp();
         bookings[i].timestamp = stamp;
         const session = driver.session();
         const ses = await session.run(`MATCH (p:Car{car_id:'${bookings[i].Car[0].id}'}) RETURN p`);
-        bookings[i].lat= await ses.records[0]._fields[0].properties.lat;
-        bookings[i].lon=await ses.records[0]._fields[0].properties.lon;
+        bookings[i].lat = await ses.records[0]._fields[0].properties.lat;
+        bookings[i].lon = await ses.records[0]._fields[0].properties.lon;
     }
     res.send(bookings);
 
 
 })
 
-app.post('/delbooking', async (req,res)=>{
+app.post('/delbooking', async (req, res) => {
     const bookid = req.body.bookid;
     await MongoFunctions.deleteBooking(bookid);
 })
 
 
-app.get('/admin', (req,res)=>{
-    
-    res.sendFile('admin.html', {root : path.join(__dirname, 'web')});
+app.get('/admin', (req, res) => {
+
+    res.sendFile('admin.html', { root: path.join(__dirname, 'web') });
 })
 
-app.get('/allcars',async (req,res)=>{
+app.get('/allcars', async (req, res) => {
     const carDetails = await MongoFunctions.allBookings('cars');
     console.log(carDetails[0]);
-    
 
 
-for(let i=0;i<carDetails.length;i++){
-    const car_id = carDetails[i].id;
-    const session = driver.session();
-    const ses = await session.run(`MATCH (p:Car{car_id:'${car_id}'}) RETURN p`);
-    delete carDetails[i]._id;
-    delete carDetails[i].photo;
-    carDetails[i].lon = ses.records[0]._fields[0].properties.lon;
-    carDetails[i].lat = ses.records[0]._fields[0].properties.lat;
-    await session.close();
-}
 
-        res.send(carDetails);
+    for (let i = 0; i < carDetails.length; i++) {
+        const car_id = carDetails[i].id;
+        const session = driver.session();
+        const ses = await session.run(`MATCH (p:Car{car_id:'${car_id}'}) RETURN p`);
+        delete carDetails[i]._id;
+        delete carDetails[i].photo;
+        carDetails[i].lon = ses.records[0]._fields[0].properties.lon;
+        carDetails[i].lat = ses.records[0]._fields[0].properties.lat;
+        await session.close();
+    }
+
+    res.send(carDetails);
 })
 
-app.post('/update',async (req,res)=>{
+app.post('/update', async (req, res) => {
     console.log(req.body);
     const result = await MongoFunctions.updateCustomer(req.body.newFname, req.body.newLname, req.body.newEmail, req.body.newPhone, req.body.userPhone);
     console.log(result);
     res.send(result);
-    
+
 })
 
 
--- web/app.js
+++ web/app.js
@@ -1,23 +1,19 @@
-let resp,userPhone,carID, carLat, carLon;
+let resp, userPhone, carID, carLat, carLon;
 let lat, lon;
-let marker=[];
-// let carMakeSelect = document.getElementById('make');
-// let carColorSelect = document.getElementById('color');
-// let carYearSelect = document.getElementById('year');
-
+let marker = [];
 var today = new Date();
-var todaysDate =today.getFullYear()+ '-'+(today.getMonth()+1).toString().padStart(2, '0')+'-'+(today.getDate()).toString().padStart(2, '0');
+var todaysDate = today.getFullYear() + '-' + (today.getMonth() + 1).toString().padStart(2, '0') + '-' + (today.getDate()).toString().padStart(2, '0');
 document.querySelector('#filterFrom').value = todaysDate;
 document.querySelector('#filterTo').value = todaysDate;
-document.querySelector('#submitButton').addEventListener('click',fetchCustomer);
+document.querySelector('#submitButton').addEventListener('click', fetchCustomer);
 let filterFrom = document.querySelector('#filterFrom').value;
 let filterTo = document.querySelector('#filterTo').value;
-document.querySelector('#filterFrom').addEventListener('change',()=>{
-    filterFrom = document.querySelector('#filterFrom').value;  
+document.querySelector('#filterFrom').addEventListener('change', () => {
+    filterFrom = document.querySelector('#filterFrom').value;
 })
 
-document.querySelector('#filterTo').addEventListener('change',()=>{
-    filterTo = document.querySelector('#filterTo').value;   
+document.querySelector('#filterTo').addEventListener('change', () => {
+    filterTo = document.querySelector('#filterTo').value;
 })
 
 
@@ -31,26 +27,26 @@ userLoc();
 
 
 //clear map before placing new markers
-function delMarkers(){
-    map.eachLayer(function(layer) {
-        if(layer._latlng!=null && layer.options.name!='home')
-         map.removeLayer(layer);
-});
+function delMarkers() {
+    map.eachLayer(function (layer) {
+        if (layer._latlng != null && layer.options.name != 'home')
+            map.removeLayer(layer);
+    });
 }
 
-async function responseProcess(lat,lon) {
+async function responseProcess(lat, lon) {
     const latlon = {};
-    latlon.lat =lat;
-    latlon.lon=lon;
-    const options =  {
+    latlon.lat = lat;
+    latlon.lon = lon;
+    const options = {
         method: 'POST',
         headers: {
             "Content-type": "Application/json"
         },
-     
+
         body: JSON.stringify(latlon)
     }
-    const rawData = await fetch('/nodes',options);
+    const rawData = await fetch('/nodes', options);
     resp = await rawData.json();
     const totCars = await resp.records;
     console.log(totCars);
@@ -58,35 +54,36 @@ async function responseProcess(lat,lon) {
         const car_id = parseInt(element._fields[2]);
         const cd = await carDet(car_id);
         lon = parseFloat(element._fields[0].x);
-        lat = parseFloat(element._fields[0].y);  
-        if(cd.carDetails[0].status!=0){  
-            const id = cd.carDetails[0].id;       
-         marker = new L.Marker([lat, lon], {
-            _icon_id: id
-        });
-        marker.addTo(map);
-        // marker.bindPopup(`car id ${marker.options._icon_id}`); //do not delete ---- understand marker options.
-        marker.bindPopup
-        (`${cd.carDetails[0].color} ${cd.carDetails[0].car_make} ${cd.carDetails[0].car_model}<br> 
+        lat = parseFloat(element._fields[0].y);
+        if (cd.carDetails[0].status != 0) {
+            const id = cd.carDetails[0].id;
+            marker = new L.Marker([lat, lon], {
+                _icon_id: id
+            });
+            marker.addTo(map);
+            // marker.bindPopup(`car id ${marker.options._icon_id}`); //do not delete ---- understand marker options.
+            marker.bindPopup
+                (`${cd.carDetails[0].color} ${cd.carDetails[0].car_make} ${cd.carDetails[0].car_model}<br> 
         Rent/day:€${cd.carDetails[0].price}<br>Distance: ${parseFloat(element._fields[1].toFixed(2))} KMs<br><img src=${cd.carDetails[0].photo}/>`)
-        marker.on('click', (e) => console.log(e.target));
-        marker.on('dblclick', async (e) => {
-            //Proceed with booking
-            document.querySelector('.filters').style.visibility='hidden';
-            carLat = e.target._latlng.lat;
-            carLon = e.target._latlng.lng;
-            carID = e.target.options._icon_id;
-            console.log(`You double clicked on ${e.target.options._icon_id}`);
-            const mapDiv = document.querySelector('#map');
-            mapDiv.style.visibility = 'hidden';
-            const inputDiv = document.querySelector('#userDetails');
-            inputDiv.style.visibility = 'visible';
-        });
-    }
+            marker.on('click', (e) => console.log(e.target));
+            marker.on('dblclick', async (e) => {
+                //Proceed with booking
+                document.querySelector('#header').style.visibility = 'hidden';
+                document.querySelector('.filters').style.visibility = 'hidden';
+                carLat = e.target._latlng.lat;
+                carLon = e.target._latlng.lng;
+                carID = e.target.options._icon_id;
+                console.log(`You double clicked on ${e.target.options._icon_id}`);
+                const mapDiv = document.querySelector('#map');
+                mapDiv.style.visibility = 'hidden';
+                const inputDiv = document.querySelector('#userDetails');
+                inputDiv.style.visibility = 'visible';
+            });
+        }
     });
 }
 async function carDet(id) {
-    const data = { id,filterFrom,filterTo };
+    const data = { id, filterFrom, filterTo };
     const options = {
         method: 'POST',
         headers: {
@@ -108,26 +105,18 @@ function userLoc() {
     const position = navigator.geolocation.getCurrentPosition((position) => {
         lat = position.coords.latitude;
         lon = position.coords.longitude;
-        map.flyTo([lat,lon],7, 'zoom');
-        L.marker([lat, lon], { name:'home', icon: myIcon, draggable:true }).on('dragend',async (e)=>{
-        // delMarkers();
-        // responseProcess(e.target._latlng.lat, e.target._latlng.lng);
-        dragFunction(e);
-    }).on('click', (e)=>console.log(e)).addTo(map).bindPopup('Your location');
-        responseProcess(lat,lon);
+        map.flyTo([lat, lon], 7, 'zoom');
+        L.marker([lat, lon], { name: 'home', icon: myIcon, draggable: true }).on('dragend', async (e) => {
+            delMarkers();
+            responseProcess(e.target._latlng.lat, e.target._latlng.lng);
+        }).on('click', (e) => console.log(e)).addTo(map).bindPopup('Your location');
+        responseProcess(lat, lon);
     });
 }
 
-function dragFunction(e){
-    delMarkers();
-        responseProcess(e.target._latlng.lat, e.target._latlng.lng);
-}
-
-
-
-async function fetchCustomer(){
+async function fetchCustomer() {
     userPhone = document.querySelector('#phone').value;
-    const phone={userPhone};
+    const phone = { userPhone };
     const options = {
         method: 'POST',
         headers: {
@@ -138,84 +127,82 @@ async function fetchCustomer(){
     const response = await fetch('/fetchuser', options);
     const userDetails = await response.json();
     console.log(userDetails.userDetails);
-    if(userDetails.userDetails==='NEW')
-        {
-            console.log('You are a new user');
-            document.querySelector('#newcustomer').style.visibility='visible';
-            document.querySelector('#newUserSub').addEventListener('click', async ()=>{
-                document.querySelector('#newUserSub').disabled = true;
-                console.log('New user');
-                const fname = document.querySelector('#fname').value;
-                const lname = document.querySelector('#lname').value;
-                const phone = userPhone;
-                const email = document.querySelector('#email').value;
-                console.log(email);
-                const userData = {fname,lname,email,phone};
-                const options = {
-                    method: 'POST',
-                    headers: {
-                        "Content-type": "Application/json"
-                    },
-                    body: JSON.stringify(userData)
-                }
+    if (userDetails.userDetails === 'NEW') {
+        console.log('You are a new user');
+        document.querySelector('#newcustomer').style.visibility = 'visible';
+        document.querySelector('#newUserSub').addEventListener('click', async () => {
+            document.querySelector('#newUserSub').disabled = true;
+            console.log('New user');
+            const fname = document.querySelector('#fname').value;
+            const lname = document.querySelector('#lname').value;
+            const phone = userPhone;
+            const email = document.querySelector('#email').value;
+            console.log(email);
+            const userData = { fname, lname, email, phone };
+            const options = {
+                method: 'POST',
+                headers: {
+                    "Content-type": "Application/json"
+                },
+                body: JSON.stringify(userData)
+            }
 
-                const resp = await fetch('/newuser',options);
-                console.log(resp.ok);
-                if(resp.ok)
+            const resp = await fetch('/newuser', options);
+            console.log(resp.ok);
+            if (resp.ok)
                 fetchCustomer();
-                document.querySelector('#newcustomer').style.visibility='collapse';
-            })
-        }
-    else
-    {
-    custBookings(userPhone);
-    const welcomeMessage = document.querySelector('#welcome');
-    welcomeMessage.innerHTML = 'Welcome Mr. ' + userDetails.userDetails.first_name +' '+userDetails.userDetails.last_name;
-    const selectedCar = document.querySelector('#selectedCar');
-    const carDets = await carDet(carID);
-    selectedCar.innerHTML = `You selected the ${carDets.carDetails[0].color} ${carDets.carDetails[0].car_make} ${carDets.carDetails[0].car_model} (${carDets.carDetails[0].year}) 
+            document.querySelector('#newcustomer').style.visibility = 'collapse';
+        })
+    }
+    else {
+        custBookings(userPhone);
+        const welcomeMessage = document.querySelector('#welcome');
+        welcomeMessage.innerHTML = 'Welcome Mr. ' + userDetails.userDetails.first_name + ' ' + userDetails.userDetails.last_name;
+        const selectedCar = document.querySelector('#selectedCar');
+        const carDets = await carDet(carID);
+        selectedCar.innerHTML = `You selected the ${carDets.carDetails[0].color} ${carDets.carDetails[0].car_make} ${carDets.carDetails[0].car_model} (${carDets.carDetails[0].year}) 
     for €${carDets.carDetails[0].price}/day`;
-    bookedDays(carDets.carDetails[0].price);
-    
-    document.querySelector('#dates').style.visibility='visible';
-    document.querySelector('#from').value = filterFrom;
-    document.querySelector('#to').value = filterTo;
-    document.querySelector('#from').disabled = true;
-    document.querySelector('#to').disabled =true;
-    document.querySelector('#submitBooking').addEventListener('click',async ()=>{
-        document.querySelector('#submitBooking').disabled = true;
-        const from = document.querySelector('#from').value;
-        const to = document.querySelector('#to').value;
-        const carPickupCode = Math.floor((Math.random()*9000)+1000);
-        const bookingData={carID,userPhone, from, to, carPickupCode};
-        const options = {
-            method: 'POST',
-            headers: {
-                "Content-type": "Application/json"
-            },
-            body: JSON.stringify(bookingData)
-        };
+        bookedDays(carDets.carDetails[0].price);
 
-        const resp = await fetch('/newbooking', options);
-        console.log(resp);
+        document.querySelector('#dates').style.visibility = 'visible';
+        document.querySelector('#from').value = filterFrom;
+        document.querySelector('#to').value = filterTo;
+        document.querySelector('#from').disabled = true;
+        document.querySelector('#to').disabled = true;
+        document.querySelector('#submitBooking').addEventListener('click', async () => {
+            document.querySelector('#submitBooking').disabled = true;
+            const from = document.querySelector('#from').value;
+            const to = document.querySelector('#to').value;
+            const carPickupCode = Math.floor((Math.random() * 9000) + 1000);
+            const bookingData = { carID, userPhone, from, to, carPickupCode };
+            const options = {
+                method: 'POST',
+                headers: {
+                    "Content-type": "Application/json"
+                },
+                body: JSON.stringify(bookingData)
+            };
 
-        const bookedCar = document.createElement('div');
-        const text = document.createElement('h4');
-        text.innerText = `Licence plate of the booked car is: ${carDets.carDetails[0].licence_plate}. Your unique pickup code is:${carPickupCode}. Your car location:`;
-        bookedCar.innerHTML = 
-        `<iframe width="500" height="350" style="border:0" loading="lazy" allowfullscreen 
+            const resp = await fetch('/newbooking', options);
+            console.log(resp);
+
+            const bookedCar = document.createElement('div');
+            const text = document.createElement('h4');
+            text.innerText = `Licence plate of the booked car is: ${carDets.carDetails[0].licence_plate}. Your unique pickup code is:${carPickupCode}. Your car location:`;
+            bookedCar.innerHTML =
+                `<iframe width="500" height="350" style="border:0" loading="lazy" allowfullscreen 
         src="https://www.google.com/maps/embed/v1/search?q=${carLat}%2C${carLon}&key=AIzaSyDDW_15VmvhkfdDWLYrPGvjvOJ9B8VIjeg">
         </iframe>`
-        document.querySelector('#userDetails').append(text);
-        document.querySelector('#userDetails').append(bookedCar);
-        await custBookings(userPhone);
-    })
+            document.querySelector('#userDetails').append(text);
+            document.querySelector('#userDetails').append(bookedCar);
+            await custBookings(userPhone);
+        })
 
     }
 }
 
-async function custBookings(userPhone){
-    const phone = {userPhone};
+async function custBookings(userPhone) {
+    const phone = { userPhone };
     const options = {
         method: 'POST',
         headers: {
@@ -223,53 +210,52 @@ async function custBookings(userPhone){
         },
         body: JSON.stringify(phone)
     };
-    const resp = await fetch('/custbookings',options);
+    const resp = await fetch('/custbookings', options);
     const bookings = await resp.json();
-    if(bookings.length!=0)
-    {
+    if (bookings.length != 0) {
+        document.getElementById('yourBooking').style.visibility = 'visible';
         document.getElementById('prevBookings').style.visibility = 'visible';
-        
+        document.getElementById('prevBookings').innerHTML = '';
     }
-    bookings.forEach(booking=>{
+    bookings.forEach(booking => {
         const detailsDiv = document.createElement('div');
         const details = document.createElement('span');
         details.innerText = `Booked on: ${booking.timestamp} Car: ${booking.Car[0].color} ${booking.Car[0].car_make} ${booking.Car[0].car_model}`
         const delButton = document.createElement('button');
         delButton.innerHTML = "Delete";
         delButton.setAttribute('bookid', `${booking._id}`)
-        delButton.addEventListener('click', async(e)=>{
+        delButton.addEventListener('click', async (e) => {
             e.target.innerHTML = 'Deleted';
             e.target.disabled = true;
 
-           const bookid = e.target.getAttribute('bookid');
-           const data = {bookid};
-           const options = {
-            method: 'POST',
-            headers: {
-                "Content-type": "Application/json"
-            },
-            body: JSON.stringify(data)
-        };
-        await fetch('/delbooking', options);
-    })
+            const bookid = e.target.getAttribute('bookid');
+            const data = { bookid };
+            const options = {
+                method: 'POST',
+                headers: {
+                    "Content-type": "Application/json"
+                },
+                body: JSON.stringify(data)
+            };
+            await fetch('/delbooking', options);
+        })
         detailsDiv.append(details);
         detailsDiv.append(delButton);
         document.getElementById('prevBookings').append(detailsDiv);
     })
 }
 
-function bookedDays(pricePerDay){
+function bookedDays(pricePerDay) {
     let from = filterFrom;
     let to = filterTo;
     let d1 = new Date(from);
     let d2 = new Date(to);
 
-    let noOfDays = (d2-d1)/(24*3600*1000);
-    if(noOfDays ===0)
-     noOfDays = 1;
-    const totalPrice = noOfDays*pricePerDay;
+    let noOfDays = (d2 - d1) / (24 * 3600 * 1000);
+    if (noOfDays === 0)
+        noOfDays = 1;
+    const totalPrice = noOfDays * pricePerDay;
     console.log(totalPrice);
     document.getElementById('proceedBooking').style.visibility = 'visible';
     document.getElementById('totalPrice').innerText = `Total price to be paid: €${totalPrice}`;
-
 }
